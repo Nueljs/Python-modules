3af22168ea73d6c7ac48ddac35563d09320845c0
Nueljs <manuelcerveralara@gmail.com>
2026-03-10 19:21:18 +0100

    fin primer modulo

--- python-module00/ex5/ft_count_harvest_iterative.py
+++ python-module00/ex5/ft_count_harvest_iterative.py
@@ -1,8 +1,7 @@
-def ft_count_harvest_iterative():
-	harvest = int(input("Days until harvest: "))
-	days = 0
-	while days < harvest:
-		days += 1
-		print(f"Day {days}")
-	if days == harvest : 
-		print("Harvest time!")
+def ft_count_harvest_iterative() -> None:
+    harvest = int(input("Days until harvest: "))
+    days = 0
+    for days in range(0, harvest):
+        days += 1
+        print(f"Day {days}")
+    print("Harvest time!")
--- python-module00/ex5/ft_count_harvest_recursive.py
+++ python-module00/ex5/ft_count_harvest_recursive.py
@@ -1,2 +1,13 @@
-def ft_count_harvest_recursive():
-	
+def count_days(harvest, day) -> None:
+    if day == harvest:
+        print(f"Day {day}")
+        return
+    else:
+        print(f"Day {day}")
+        count_days(harvest, day + 1)
+
+
+def ft_count_harvest_recursive() -> None:
+    harvest = int(input("Days until harvest: "))
+    count_days(harvest, 1)
+    print("Harvest time!")
