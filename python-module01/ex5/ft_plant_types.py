92220714857eb66e9b3ea4cbcb9af616525aaa17
Nueljs <manuelcerveralara@gmail.com>
2026-03-21 18:01:42 +0100

    a

--- python-module01/ex5/ft_plant_types.py
+++ python-module01/ex5/ft_plant_types.py
@@ -4,24 +4,35 @@ class Plant:
     def __init__(self, name: str, height: int, age: int) -> None:
         self.name: str = name
         self.height: int = height
-        self.age: int = age
+        self._age: int = age
 
-    def get_info(self) -> str:
+    def show(self) -> None:
         type: str = self.__class__.__name__
-        return (f"{self.name} ({type}): {self.height}cm, {self.age} days")
+        print(f"{self.name} ({type}): {self.height}cm, {self._age} days")
 
 
 class Flower(Plant):
     def __init__(self, name: str, height: int, age: int, color: str) -> None:
         super().__init__(name, height, age)
         self.color: str = color
+        self.is_blooming: bool = False
 
     def bloom(self) -> str:
+        self.is_blooming = True
         return (f"{self.name} is blooming beautifully!")
 
-    def get_info(self) -> str:
-        return (f"{super().get_info()}, {self.color} color \n"
-                f"{self.bloom()}")
+    def show(self) -> None:
+        if not self.is_blooming:
+            print(f"{self.name}: {self.height:.1f}cm, {self._age} days old\n"
+                  f"Color: {self.color}")
+            print("Rose has not bloomed yet")
+            print("[asking the rose to bloom]")
+            self.bloom()
+            self.show()
+        elif self.is_blooming:
+            print(f"{self.name}: {self.height:.1f}cm, {self._age} days old\n"
+                  f"Color: {self.color}\n"
+                  f"{self.name} is bloomign beautifully!")
 
 
 class Tree(Plant):
@@ -31,37 +42,48 @@ class Tree(Plant):
         self.trunk_diameter: int = trunk_diameter
 
     def produce_shade(self) -> str:
-        return (f"{self.name} provide 78 square meters of shade")
+        return (f"[asking the {self.name} to produce shade]\n"
+                f"{self.__class__.__name__} {self.name} now produces a shade"
+                " of 200.0cm long and 5.0cm wide.\n")
 
-    def get_info(self) -> str:
-        return (f"{super().get_info()} {self.trunk_diameter}cm diameter \n"
-                f"{self.produce_shade()}")
+    def show(self) -> None:
+        print(f"{self.name}: {self.height:.1f}cm, {self._age} days old\n"
+              f" Trunk diameter: {self.trunk_diameter:.1f}cm")
+        print(self.produce_shade())
 
 
 class Vegetable(Plant):
     def __init__(self, name: str, height: int, age: int, harvest_season: str,
-                 nutritional_value: str) -> None:
+                 nutritional_value: int) -> None:
         super().__init__(name, height, age)
         self.harvest_season: str = harvest_season
-        self.nutritional_value: str = nutritional_value
+        self.nutritional_value: int = 0
 
-    def get_info(self) -> str:
-        return (f"{super().get_info()}, {self.harvest_season} harvest \n"
-                f"{self.name} is rich in {self.nutritional_value}")
+    def age(self) -> None:
+        pass
+
+    def grow(self) -> None:
+        pass
+
+    def show(self) -> None:
+        print(f"{super().show()}\n"
+              f" Harvest season: {self.harvest_season}\n"
+              f" Nutrtional value: {self.nutritional_value}"
+              f"{self.name} is rich in {self.nutritional_value}")
+        print(f"[make {self.name} grow and age for 20 days]")
 
 
 def ft_plant_types() -> None:
-    rose = Flower("Rose", 25, 30, "red")
-    daisy = Flower("Daisy", 18, 15, "white")
-    oak = Tree("Oak", 500, 1825, 50)
-    pine = Tree("Pine", 700, 1345, 48)
-    tomato = Vegetable("Tomato", 80, 90, "summer", "vitamin C")
-    carrot = Vegetable("Carrot", 45, 60, "autum", "vitamin K")
-    plants: list = [rose, daisy, oak, pine, tomato, carrot]
-    for i in range(6):
-        print(f"{plants[i].get_info()} \n")
+    rose = Flower("Rose", 15, 10, "red")
+    oak = Tree("Oak", 200, 365, 5)
+    tomato = Vegetable("Tomato", 5, 10, "April", 0)
+    plants: list = [rose, oak, tomato]
+    for plant in plants:
+        print(f"=== {plant.__class__.__name__}")
+        plant.show()
+        print("\n")
 
 
 if __name__ == "__main__":
-    print(" === Garden Plant Types === \n")
+    print("=== Garden Plant Types ===")
     ft_plant_types()
