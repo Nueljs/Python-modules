e38b08fe0f19f06aed16229e6520e0d869b7ac14
Nueljs <manuelcerveralara@gmail.com>
2026-03-30 20:59:17 +0200

    ex5

--- python-module03/ex5/ft_data_stream.py
+++ python-module03/ex5/ft_data_stream.py
@@ -12,6 +12,29 @@ def gen_event() -> typing.Generator[tuple[str, str], None, None]:
         yield (event)
 
 
+def consume_event(events: list) -> typing.Generator[tuple[str, str],
+                                                    None, None]:
+    while len(events) > 0:
+        i: int = random.randrange(len(events))
+        event = events[i]
+        events[:] = events[:i] + events[i + 1:]
+        yield event
+
+
 def ft_data_stream() -> None:
     my_generator: typing.Generator[tuple[str, str], None, None] = gen_event()
-    event: tuple[str, str] = next(my_generator)
+    for i in range(1000):
+        event: tuple[str, str] = next(my_generator)
+        print(f"Event {i}: Player {event[0]} did action {event[1]}")
+    ten_events: list[tuple[str, str]] = [next(my_generator) for _ in range(10)]
+    print(f"Built list of 10 events: {ten_events}")
+    extract_event: typing.Generator[
+        tuple[str, str], None, None] = consume_event(ten_events)
+    for event in extract_event:
+        print(f"Got event from list: {event}")
+        print(f"Remain in list: {ten_events}")
+
+
+if __name__ == "__main__":
+    print("=== Game Data Stream Processor ===")
+    ft_data_stream()
