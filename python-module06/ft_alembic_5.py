79e874517b208b7f8d2d2af5536983232fd489ee
Nueljs <manuelcerveralara@gmail.com>
2026-04-11 20:03:05 +0200

    a

--- python-module06/ft_alembic_5.py
+++ python-module06/ft_alembic_5.py
@@ -1,3 +1,6 @@
 from alchemy import elements
 
-elements.create_air()
+if __name__ == "__main__":
+    print("=== Alembic 5 ===")
+    print("Accessing the alchemy module using 'from alchemy import ...'")
+    print(f"Testing create_air: {elements.create_air()}")
